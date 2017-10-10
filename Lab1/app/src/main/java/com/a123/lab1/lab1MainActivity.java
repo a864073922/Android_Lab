540ceb21538528a2bdd903186dba52a309ab55ce
a864073922 <a864073922@users.noreply.github.com>
2017-10-10 22:25:41 +0800

    update

---
+++ Lab1/app/src/main/java/com/a123/lab1/lab1MainActivity.java
@@ -0,0 +1,13 @@
+package com.a123.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lab1MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab1_main);
+    }
+}
---
+++ Lab1/app/src/main/java/lab1.java
@@ -0,0 +1,6 @@
+/**
+ * Created by 邹武逸 on 2017/9/25.
+ */
+
+public class lab1 {
+}
